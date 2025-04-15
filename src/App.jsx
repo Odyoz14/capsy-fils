f34417055c7a8c05297cb7015ff9b43d06a32f00
Odyoz14 <alexcrypto14@gmail.com>
2025-04-15 15:37:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
-export default function App() {
+ function App() {
   const capsuleRefs = useRef([]);
   const [fils, setFils] = useState([]);
 
